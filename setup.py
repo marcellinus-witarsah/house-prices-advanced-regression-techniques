c47a2f8a269b7181305cea0916277df23a82516a
marcellinus-witarsah <aw.marcel@gmail.com>
2023-08-24 21:12:04 +0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Personal project to get my hands on advanced regression techniques',
+    author='Marcellinus Aditya Witarsah',
+    license='MIT',
+)
